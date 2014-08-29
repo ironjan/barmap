84bd45f1633493898753e01a4d5a2235d4471c30
Jan Lippert <ljan@mail.upb.de>
2014-08-29 22:13:23 +0200

    Initialized heroku template

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/barmap-standalone.jar clojure.main -m barmap.web
---
+++ test/barmap/web_test.clj
@@ -0,0 +1,6 @@
+(ns barmap.web-test
+  (:require [clojure.test :refer :all]
+            [barmap.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
