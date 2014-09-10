9f9302f2502ea8d1fca2351956e0115c96adeaad
Jan Lippert <lippertsjan@gmail.com>
2014-09-10 13:42:16 +0200

    Added dep

--- project.clj
+++ project.clj
@@ -12,7 +12,8 @@
                  [com.cemerick/drawbridge "0.0.6"]
                  [org.clojure/java.jdbc "0.3.2"]
                  [postgresql "9.1-901.jdbc4"]
-                 [hiccup "1.0.4"]]
+                 [hiccup "1.0.4"]
+                 [ring-middleware-format "0.2.2"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
