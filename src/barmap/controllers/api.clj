fa59f0bae835b5dd54a022681b3793dfad37116d
Jan Lippert <lippertsjan@gmail.com>
2014-09-21 19:48:22 +0200

    get via json works locally

--- src/barmap/controllers/api.clj
+++ src/barmap/controllers/api.clj
@@ -1,9 +1,12 @@
 (ns barmap.controllers.api
   (:use [ring.util.response])
-  (:require [compojure.core :refer [defroutes context OPTIONS]]
-            [ring.util.response :as ring]))
+  (:require [compojure.core :refer [defroutes context OPTIONS GET PUT POST DELETE
+                                    ]]
+            [ring.util.response :as ring]
+            [barmap.model.places :as places]))
 
 
 (defroutes routes
            (context "/api" []
+                    (GET "/" [] (-> (response (places/all)) ))
                     (OPTIONS "/" [] (-> (response {:version "0.1.0-SNAPSHOT"}) (header "Allow" "OPTIONS")))))
