2c8e1821574591ddad2a6db86ff77b25b8008b21
Jan Lippert <lippertsjan@gmail.com>
2014-09-21 20:38:48 +0200

    corrected error

--- src/lobos/config.clj
+++ src/lobos/config.clj
@@ -2,7 +2,7 @@
   (:use lobos.connectivity)
   (:require [heroku-database-url-to-jdbc.core :as heroku-url]))
 
-(def db (or (heroku-url (System/getenv "DATABASE_URL"))
+(def db (or (heroku-url/jdbc-connection-string (System/getenv "DATABASE_URL"))
   {:classname "org.postgresql.Driver"
    :subprotocol "postgresql"
    :subname "//localhost:5432/barmap"}))
