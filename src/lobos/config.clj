c953baf312933e3a224f18ac8db4b3275b8f90e2
Jan Lippert <lippertsjan@gmail.com>
2014-09-21 20:01:32 +0200

    using environment var or local conf

--- src/lobos/config.clj
+++ src/lobos/config.clj
@@ -1,9 +1,9 @@
 (ns lobos.config
   (:use lobos.connectivity))
 
-(def db
+(def db (or (System/getenv "DATABASE_URL")
   {:classname "org.postgresql.Driver"
    :subprotocol "postgresql"
-   :subname "//localhost:5432/barmap"})
+   :subname "//localhost:5432/barmap"}))
 
 (open-global db)
