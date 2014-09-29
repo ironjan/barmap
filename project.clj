3382eb733c1fa497925c03ae113eb653e8cfd48f
Jan Lippert <lippertsjan@gmail.com>
2014-09-29 19:09:06 +0200

    Added org.clojure/tools.nrepl to dependencies
    herokue lein repl was timing out because it could not find a class
    contained in that dependency

--- project.clj
+++ project.clj
@@ -16,7 +16,8 @@
                  [ring-middleware-format "0.2.2"]
                  [lobos "1.0.0-beta3"]
                  [postgresql "9.3-1102.jdbc41"]
-                 [heroku-database-url-to-jdbc "0.2.2"]]
+                 [heroku-database-url-to-jdbc "0.2.2"]
+                 [org.clojure/tools.nrepl "0.2.5"]]
   :main barmap.web
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
