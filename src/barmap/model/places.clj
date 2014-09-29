2d92a8be173d4dadccd0c728b33e3146d9e5216e
Jan Lippert <lippertsjan@gmail.com>
2014-09-29 20:05:48 +0200

    Fixed a bug (db was not called as method)

--- src/barmap/model/places.clj
+++ src/barmap/model/places.clj
@@ -3,7 +3,7 @@
             [lobos.config :as config]))
 
 (defn all []
-  (into [] (sql/query config/db ["select * from places;"])))
+  (into [] (sql/query (config/db) ["select * from places;"])))
 
 (defn allViaType [type]
-  (into [] (sql/query config/db ["select * from places where type_id = ?" type])))
+  (into [] (sql/query (config/db) ["select * from places where type_id = ?" type])))
