8dc83689fd00dd90da0f3b1db02613fff539a3b3
Jan Lippert <lippertsjan@gmail.com>
2014-10-10 22:27:59 +0200

    Fixed problem with url settings

--- src/lobos/config.clj
+++ src/lobos/config.clj
@@ -2,12 +2,17 @@
   (:use [lobos.connectivity]
         [heroku-database-url-to-jdbc.core]))
 
+(def localDb
+  {:classname   "org.postgresql.Driver"
+   :subprotocol "postgresql"
+   :subname     "//localhost:5432/barmap"})
+
+
 (defn db []
-;; FIXME korma-connection-map throws NPE if DATABASE_URL is not set
-  (or (korma-connection-map (System/getenv "DATABASE_URL"))
-              {:classname   "org.postgresql.Driver"
-               :subprotocol "postgresql"
-               :subname     "//localhost:5432/barmap"}))
+  (if-let
+      [url (System/getenv "DATABASE_URL")]
+    (korma-connection-map url)
+    localDb))
 
 (defn init []
   (open-global (db)))
